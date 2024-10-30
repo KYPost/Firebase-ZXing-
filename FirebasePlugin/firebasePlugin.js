dc3ffb7652bb6019d39d67574c284ac313bf0177
KYPost <kypostdesign@gmail.com>
2024-10-30 16:51:27 +0800

    initial commit

---
+++ FirebasePlugin/firebasePlugin.js
@@ -0,0 +1,14 @@
+// Firebase的金鑰
+const firebaseConfig = {
+    apiKey: "AIzaSyBWeqlKFZWzT6GQrALiPfNI_0kqPIVnR-I",
+  authDomain: "fir-zxing-test.firebaseapp.com",
+  databaseURL: "https://fir-zxing-test-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fir-zxing-test",
+  storageBucket: "fir-zxing-test.appspot.com",
+  messagingSenderId: "1088581410469",
+  appId: "1:1088581410469:web:7bcc8bb4e8e84bb826374c",
+  measurementId: "G-WJ08215WTQ"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
